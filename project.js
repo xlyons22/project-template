a9ccf69fc43bc385fd79121b6026179c8f1d7c1a
EWB User <ewb@unassigned-hostname>
2014-04-22 17:25:30 -0400

    charater

--- project.js
+++ project.js
@@ -1,19 +1,22 @@
-title('My Project');
+title('Clown Killer');
 
 // put variables here, just like kahn academy
 var exampleVar = 100;
 
 // put functions here, just like kahn academy
-var exampleFunction = function() {
-    
-    // change color depending on key pressed
-    if (keyIsPressed && keyCode === SPACE) {
-        fill(255, 0, 0);
-    } else {
-        fill(0, 0, 255);
-    }
+var drawclown = function() {
+	fill(23,32,3);
+	ellipse(400,600,150,42);
+	ellipse(400,543,75,75);
+	noStroke();
+	fill(250,255,250);
+	ellipse(400,543,50,50);
+	ellipse(400,510,40,40);
+	ellipse(400,480,30,30);
+	ellipse
+	ellipse
+	ellipse
 
-    rect(300, 200, 55, 55);
 };
 
 // this code is executed once when the program is started
@@ -21,26 +24,17 @@ var setup = function() {
 
     // set up the size of the canvas (you probably don't want to change this!)
     size(800, 600);
-
 };
 
 // override draw function, by default it will be called 60 times per second
 var draw = function() {
 
     // clear the screen
-    background(255, 255, 255);
-
+    background(240,10,10);
     // some drawing commands; feel free to change these
-    fill(255, 0, 0);
-
-    // draw some text
-    textSize(50);
-    text("Hello, World!", exampleVar, exampleVar);
-
-    textSize(20);
-    text("Press the space bar...", 500, 500);
-
-    // call a function (defined up above)
-    exampleFunction();
+    fill(255, 320,0)
+    // call a functon (defined up above)
+    drawclown();
+	
 
 };
