877c0830dcb5d6c374d46ec6702173440c7bd36c
EWB User <ewb@unassigned-hostname>
2014-04-29 17:14:24 -0400

    it works

--- project.js
+++ project.js
@@ -1,21 +1,52 @@
-title('Clown Killer');
+ title('Clown Killer');
 
-// put variables here, just like kahn academy
-var exampleVar = 100;
+//put variables here, just like khan academy
+var clownX = 400;
+
+var knifeY1 = 560;
+var knifeThrown1 = 0;
+var knifeX1 = 900;
+
+   
+var knifeY2 = 560;
+var knifeThrown2 = 0;
+var knifeX2 = 900;
+
+
+var knifeY3 = 560;
+var knifeThrown3 = 0;
+var knifeX3 = 900;
+
+var OK2Fire = 1;
+
 
 // put functions here, just like kahn academy
 var drawclown = function() {
+	//knife moving1
+	fill(219,227,227);
+	rect(knifeX1, knifeY1, 5, 30);
+	fill(255,0,255);
+	rect(knifeX2, knifeY2, 5, 30);
+	fill(0,45,234);
+	rect(knifeX3, knifeY3, 5, 30);
+	
+	//knife fake
+	rect(clownX+50,560,5,30);
+	
+
+	//clown
 	fill(23,32,3);
-	ellipse(400,600,150,42);
-	ellipse(400,543,75,75);
+	ellipse(clownX,600,150,42);
+	ellipse(clownX,543,75,75);
 	noStroke();
 	fill(250,255,250);
-	ellipse(400,543,50,50);
-	ellipse(400,510,40,40);
-	ellipse(400,480,30,30);
-	ellipse
-	ellipse
-	ellipse
+	ellipse(clownX,543,50,50);
+	ellipse(clownX,510,40,40);
+	ellipse(clownX,480,30,30);
+	ellipse(clownX+30,543,40,40);
+	ellipse(clownX+50,543,30,30);
+	ellipse(clownX-30,543,40,40);
+	ellipse(clownX-50,543,30,30);
 
 };
 
@@ -35,6 +66,72 @@ var draw = function() {
     fill(255, 320,0)
     // call a functon (defined up above)
     drawclown();
+
+    if (keyIsPressed) {
+	if (keyCode === 65) {
+		clownX -= 5;
+        }
+	if (keyCode === 68  ) {
+		clownX += 5;
+	} 
+	//space bar
+	if (keyCode === 32)  {
+		if (OK2Fire === 1){
+			OK2Fire = 2;
+
+			if (knifeThrown1===0){
+				knifeThrown1 = 1;
+				knifeX1 = clownX;
+
+			}else if (knifeThrown2===0){
+				knifeThrown2 = 1;
+				knifeX2 = clownX;
+
+			}else if (knifeThrown3===0){
+				knifeThrown3 = 1;
+				knifeY3 = clownX;
+			}
+		}
+	}
 	
+	
+}
+
+	if (OK2Fire > 1){
+		OK2Fire += 1;
+
+	}
+	if (OK2Fire > 10){
+		OK2Fire = 1;
+
+	}
+
+	if (knifeThrown1 === 1){
+		knifeY1 -= 7;
+		if (knifeY1<-16){
+			knifeThrown1=0;
+			knifeY1=560;
+		}
+	}
+
+	if (knifeThrown2 === 1){
+		knifeY2 -= 7;
+		if (knifeY2<-16){
+			knifeThrown2=0;
+			knifeY2=560;
+		}
+	}
+
+	if (knifeThrown3 === 1){
+		knifeY3 -= 7;
+		if (knifeY3<-16){
+			knifeThrown3=0;
+			knifeY3=560;
+		}
+	}
+
+
+
+
 
 };
